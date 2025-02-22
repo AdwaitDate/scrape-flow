07f0a547be5124777114498e6509e61e15463ee2
AdwaitDate <adwaitdate2018@gmail.com>
2025-02-23 03:22:36 +0530

    Successfully implemented workflow deletion task y-1:42:24

--- app/(dashboard)/workflows/_components/DeleteWorkflowDialog.tsx
+++ app/(dashboard)/workflows/_components/DeleteWorkflowDialog.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { DeleteWorkflow } from '@/actions/workflows/deleteWorkflow';
 import {
     AlertDialog,
     AlertDialogAction,
@@ -10,28 +11,56 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger
 } from '@/components/ui/alert-dialog';
+import { Input } from '@/components/ui/input';
+import { useMutation } from '@tanstack/react-query';
 
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'sonner';
 
 
 interface Props{
     open:boolean;
     setOpen:(open:boolean)=>void;
     workflowName:string;
+    workflowId:string;
 }
-function DeleteWorkflowDialog({open,setOpen, workflowName}:Props) {
+function DeleteWorkflowDialog({open,setOpen, workflowName,workflowId}:Props) {
+    const [confirmText, setConfirmText] = useState('');
+    const deleteMutation = useMutation({
+        mutationFn:DeleteWorkflow,
+        onSuccess:()=>{
+            toast.success("Workflow deleted successfully",{id:workflowId});
+            setConfirmText('');
+        },
+        onError:()=>{
+            toast.error("something went wrong",{id:workflowId});
+        }
+    })
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
     <AlertDialogContent>
-        <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+        <AlertDialogHeader className='gap-4'>
+            <AlertDialogTitle className='text-center'>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
                 If you delete this workflow, you will not be able to recover it.
-                <div className="flex flec-col py-4 gap-2">
+                <div className="flex flex-col py-4 gap-4">
                     <p>If you are sure, enter <b>{workflowName}</b> to confirm</p>
+                    <Input value={confirmText} onChange={(e)=> setConfirmText(e.target.value)}/>
                 </div>
             </AlertDialogDescription>
         </AlertDialogHeader>
+        <AlertDialogFooter>
+            <AlertDialogCancel onClick={()=> setConfirmText("")}>Cancel</AlertDialogCancel>
+            <AlertDialogAction disabled={confirmText!=workflowName || deleteMutation.isPending}
+            className='bg-destructive text-destructive-foreground hover:bg-destructive/90'
+            onClick={(e)=>{
+              
+                toast.loading("Deleting workflow...",{id:workflowId});
+                deleteMutation.mutate(workflowId);
+            }}
+            >
+                Delete</AlertDialogAction>
+        </AlertDialogFooter>
     </AlertDialogContent>
     </AlertDialog>
   )
